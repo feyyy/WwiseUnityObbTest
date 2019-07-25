2cb929aa323b46e5225aaf5d8cbbaed10a90d7ee
ozgurtaskin <ozgurtaskincs@gmail.com>
2019-07-25 23:43:43 +0300

    initial commit

---
+++ Assets/TestTools/TestUIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AK.Wwise;
+
+public class TestUIManager : MonoBehaviour {
+	
+	public void LoadBank(string name) {
+		AkBankManager.LoadBank(name, false, false);
+	}
+	public void UnloadBank(string name) {
+		AkBankManager.UnloadBank(name);
+	}
+	public void PlayEvent(string name) {
+		AkSoundEngine.PostEvent(name, gameObject);
+	}
+}
